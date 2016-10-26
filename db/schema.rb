5bb9cf7029295226748f8478ecb1bef6cb8fa99e
Jon Kimble <jonathanbkimble@gmail.com>
2016-10-25 22:32:42 -0600

    add password and email to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026012033) do
+ActiveRecord::Schema.define(version: 20161026042919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20161026012033) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
+    t.string   "password"
   end
 
   add_foreign_key "ideas", "categories"
